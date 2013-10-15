2fc038f25a7d4cae450e50bda44c6eb00d1f5c75
MathCraft <mathcraft.org@gmail.com>
2013-10-15 16:50:47 -0500

    + worked on code that  talks to remote database (partial success)

--- src/notate.js
+++ src/notate.js
@@ -9,6 +9,7 @@ var connection = mysql.createConnection({
   host     : 'mysql.mathcraft.org',
   user     : 'notateapp',
   password : 'Ypzyfl3.14',
+  database : 'wikipedia_en_experiment'
 });
 
 connection.connect(function(err) {
@@ -19,8 +20,14 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
     if (err) throw err;
 
       console.log('The solution is: ', rows[0].solution);
-    });
+});
 
+
+connection.query('SELECT page_title FROM page LIMIT 1', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('The selected data is: ', rows[0].solution);
+});
 
 connection.end(function(err) {
     // The connection is terminated now
